f6b5d52ffe17c01f990a09093052b00f82da0cd8
Chris Collins <kuroneko@sysadninjas.net>
2022-01-22 22:31:53 +1100

    simple SDL hello world using MessageBox.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+#include <SDL.h>
+#include <SDL_main.h>
+
 int
-main()
+main(int argc, char **argv)
 {
     std::cout << "Hello, World!" << std::endl;
+
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Hello World", nullptr);
+
     return 0;
 }
